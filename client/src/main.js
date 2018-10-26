8a19487c36358db87e2fe36930635999f10f279f
Patrick Khuu <khuupatrick91@gmail.com>
2018-10-25 23:43:57 -0400

    Revert "Merge branch 'master' into Feature-3139"
    This reverts commit 2c849c4b796a945529e8e79182f4a03ca92ca6b0, reversing
    changes made to 194eac3033f16ba900e22bd76455fc847ff0d66b.

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,8 @@
-//https://router.vuejs.org/guide/#html
-
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  router,
   render: h => h(App)
 }).$mount('#app')
