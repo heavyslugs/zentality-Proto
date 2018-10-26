2f0876bbd8e29d67c1df487dec5ee2b3d2e2890a
james2ce <cjames96.uc@gmail.com>
2018-10-25 21:48:00 -0400

    Edited some pages. Should be ready to push into master.

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,7 @@
 //Added this entire file!
 import Vue from 'vue'
 import Router from 'vue-router'
-import App from '@/App.vue'
+// import App from '@/App.vue'
 import Home from '@/components/Home.vue'
 import Analytics from '@/components/Analytics.vue'
 import Resources from '@/components/Resources.vue'
@@ -11,13 +11,13 @@ Vue.use(Router)
 
 export default new Router ({
     routes: [
+        // {
+        //     path: '/',
+        //     name: 'App',
+        //     component: App
+        // },
         {
             path: '/',
-            name: 'App',
-            component: App
-        },
-        {
-            path: '/home',
             name: 'Home',
             component: Home
         },
